3738b51879aad728251fa164ddc4a72eefd2b008
jaimemoray <jaime_moray@hotmail.com>
2024-12-10 21:40:24 +0100

    Touch Example

--- contactless_button.ino
+++ contactless_button.ino
@@ -1,9 +1,18 @@
+
+// ESP32 WROOM Dev
+#include <driver/touch_pad.h>
+
+const int touchPin=4;
+
 void setup() {
+  Serial.begin(115200);
+  delay(1000);
   // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println(touchRead(touchPin));
+  delay(1000);
 
 }
